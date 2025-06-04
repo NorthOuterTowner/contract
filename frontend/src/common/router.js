2fade52840672cf38af7043d79e7f794ef148e63
ZXY7112 <144028624+ZXY7112@users.noreply.github.com>
2025-06-05 02:36:08 +0800

    Revert "初步实现功能对页面访问的限制，正在等待与登陆状态作者对接"

--- frontend/src/common/router.js
+++ frontend/src/common/router.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import { routerGuard } from '../common/permission.js';
 
-// 导入各个视图组件
+//import test from '../views/test.vue';
 import FirstPage from '../views/FirstPage.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
@@ -30,15 +29,24 @@ import PermissionManagement from '../views/PermissionManagement.vue';
 import AssignPermissions from '../views/AssignPermission.vue'; 
 import SignContract from '../views/SignContract.vue';
 import SignContent from '../views/signContent.vue';
+import { compile } from 'vue';
 
-let routes = [
-    { path: '/', redirect: '/FirstPage' },
-    { path: '/FirstPage', component: FirstPage },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
-    { path: '/HomePage', component: HomePage },
-    { path: "/approveList", component: approveList },
-    { path: '/approval', component: approval },
+let routes= [
+
+  
+   { path:'/',redirect:'/FirstPage'},
+    { path:'/FirstPage',component : FirstPage},
+   // { path:'/',redirect:'Login'},
+    { path:'/login',component:Login},
+    
+    //{ path:'/',redirect:'/Register'},
+    { path:'/register',component:Register},
+
+
+    //{ path: '/', redirect: '/HomePage'},
+    { path: '/HomePage', component :HomePage},
+    { path: "/approveList", component:approveList },
+    { path: '/approval', component:approval },
     { path: "/DraftContract", component: DraftContract },
     { path: "/DraftContractList", component: DraftContractList },
     { path: "/CoSignContract", component: CoSignContract },
@@ -46,30 +54,27 @@ let routes = [
     { path: "/CoSignContractList", component: CoSignContractList },
     { path: "/FinalizeContract", component: FinalizeContract },
     { path: "/FinalizeContractList", component: FinalizeContractList },
-    { path: '/PendingContractList', component: PendingContractList },
-    { path: '/allocate/:contractId', component: AssignContract },
-    { path: "/approve/content", component: content },
-    { path: '/user', component: UserManagement, meta: { requiredPermissions: ['查询用户'] } },
-    { path: '/user/add', component: AddUser, meta: { requiredPermissions: ['新增用户'] } },
-    { path: '/user/modify/:userId', component: ModifyUser, meta: { requiredPermissions: ['编辑用户'] } },
-    { path: '/role', component: RoleManagement, meta: { requiredPermissions: ['查询角色'] } },
-    { path: '/role/add', component: AddRole, meta: { requiredPermissions: ['新增角色'] } },
-    { path: '/role/modify/:roleId', component: ModifyRole, meta: { requiredPermissions: ['编辑角色'] } },
-    { path: '/function', component: FunctionManagement, meta: { requiredPermissions: ['查询功能'] } },
-    { path: '/function/add', component: AddFunction, meta: { requiredPermissions: ['新增功能'] } },
-    { path: '/permission', component: PermissionManagement, meta: { requiredPermissions: ['配置权限'] } },
-    { path: '/permission/assign/:userId', component: AssignPermissions, meta: { requiredPermissions: ['配置权限'] } },
+    { path: '/PendingContractList',component: PendingContractList },
+    { path: '/allocate/:contractId',component: AssignContract },
+    { path: "/approve/content",component:content },
+    { path: '/user', component: UserManagement },
+    { path: '/user/add', component: AddUser },
+    { path: '/user/modify/:userId', component: ModifyUser },
+    { path: '/role', component: RoleManagement },
+    { path: '/role/add', component: AddRole},
+    { path: '/role/modify/:roleId', component: ModifyRole},
+    { path: '/function', component: FunctionManagement },
+    { path: '/function/add', component: AddFunction },
+    { path: '/permission', component: PermissionManagement },
+    { path: '/permission/assign/:userId', component: AssignPermissions },
     { path: "/query", component: QueryPage },
-    { path: "/SignContractList", component: SignContract },
+    { path: "/SignContractList", component: SignContract},
     { path: "/sign/content", component: SignContent },
-];
-
+]
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+  history: createWebHashHistory(),
+  routes
 });
 
-// 使用全局前置守卫
-router.beforeEach(routerGuard);
-
 export { router, routes };
+
